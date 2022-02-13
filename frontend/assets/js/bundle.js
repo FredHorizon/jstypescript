dcce5b797937458c809c0103ec3333d80ac6f6ed
FredHorizon <fredoliveira.sys@gmail.com>
2022-02-13 13:44:38 -0400

    276. Exercício - Validando um formulário (config hide error message)

--- frontend/assets/js/bundle.js
+++ frontend/assets/js/bundle.js
@@ -9,7 +9,30 @@
 /***/ (() => {
 
 
-console.log('teste1');
+const SHOW_ERROR_MESSAGES = 'show-error-message';
+const form = document.querySelector('.form');
+const username = document.querySelector('.username');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const password2 = document.querySelector('.password2');
+// Aqui o typescript faz a inferência automaticamente no parâmetro event
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    hideErrorMessages(this);
+});
+function hideErrorMessages(form) {
+    form
+        .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+        .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+function showErrorMessage(input, msg) {
+    const formFields = input.parentElement;
+    const errorMessage = formFields.querySelector('.error-message');
+    errorMessage.innerText = msg;
+    formFields.classList.add(SHOW_ERROR_MESSAGES);
+}
+showErrorMessage(username, 'Preencher nome de usuário.');
+hideErrorMessages(form);
 
 
 /***/ })
--- src/A019-exercise/form-control.ts
+++ src/A019-exercise/form-control.ts
@@ -1,1 +1,31 @@
-console.log('teste1');
+const SHOW_ERROR_MESSAGES = 'show-error-message';
+
+const form = document.querySelector('.form') as HTMLFormElement;
+const username = document.querySelector('.username') as HTMLInputElement;
+const email = document.querySelector('.email') as HTMLInputElement;
+const password = document.querySelector('.password') as HTMLInputElement;
+const password2 = document.querySelector('.password2') as HTMLInputElement;
+
+// Aqui o typescript faz a inferência automaticamente no parâmetro event
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  hideErrorMessages(this);
+});
+
+function hideErrorMessages(form: HTMLFormElement): void {
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
+  errorMessage.innerText = msg;
+  formFields.classList.add(SHOW_ERROR_MESSAGES);
+}
+
+showErrorMessage(username, 'Preencher nome de usuário.');
+hideErrorMessages(form);
